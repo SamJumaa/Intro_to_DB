d6640bbcf12c0d9410faf1f228d70f265fdd32a8
Samy Juma <info@samjuma.co.ke>
2025-12-17 21:55:31 +0300

    remove spaces

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -52,7 +52,7 @@ mycursor.execute ("""
     CREATE TABLE Order_Details(
         orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
         order_id INT  FOREIGN KEY (order_id) REFERENCES Orders(order_id),
-        book_id INT  FOREIGN KEY(book_id) REFERENCES Books(book_id),
+        book_id INT  FOREIGN KEY (book_id) REFERENCES Books(book_id),
         quantity DOUBLE
     )
     
