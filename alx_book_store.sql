74c51ff27821e361b40cff62218562469a072019
Samy Juma <info@samjuma.co.ke>
2025-12-17 21:46:25 +0300

    remove space

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -41,9 +41,9 @@ mycursor.execute ("""
 mycursor.execute ("""
     CREATE TABLE Orders(
         order_id INT AUTO_INCREMENT PRIMARY KEY,
-        customer_id INT,
+        customer_id INT  FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
         order_date DATE,
-        FOREIGN KEY (customer_id) REFERENCES Customers (customer_id)
+       
     )
     
 """)  
