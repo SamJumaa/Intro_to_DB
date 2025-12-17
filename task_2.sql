5c9571fb793213d5a83972802d739a15766d3c57
Samy Juma <info@samjuma.co.ke>
2025-12-18 00:15:23 +0300

    Add task_2.sql with all tables for alx_book_store

---
+++ task_2.sql
@@ -0,0 +1,61 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(host="localhost", user= "root", password= "Barshosho@047")
+
+mycursor = mydb.cursor()
+mycursor.execute ("CREATE DATABASE IF NOT EXISTS alx_book_store")
+mycursor.execute ("USE alx_book_store")
+
+mycursor.execute ("""
+    CREATE TABLE Authors(
+        author_id INT AUTO_INCREMENT PRIMARY KEY,
+        author_name VARCHAR(215)
+
+    )
+
+""")
+
+mycursor.execute ("""
+    CREATE TABLE Books(
+        book_id INT AUTO_INCREMENT PRIMARY KEY,
+        title VARCHAR(130),
+        author_id INT,
+        price DOUBLE,
+        publication_date DATE,
+        FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+
+    )
+    
+""")
+
+mycursor.execute ("""
+    CREATE TABLE Customers(
+        customer_id INT AUTO_INCREMENT PRIMARY KEY,
+        customer_name VARCHAR(215),
+        email VARCHAR(215),
+        address TEXT
+    )
+
+""")    
+
+mycursor.execute ("""
+    CREATE TABLE Orders(
+        order_id INT AUTO_INCREMENT PRIMARY KEY,
+        customer_id INT  FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
+        order_date DATE,
+       
+    )
+    
+""")  
+
+mycursor.execute ("""
+    CREATE TABLE Order_Details(
+        orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
+        order_id INT  FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+        book_id INT  FOREIGN KEY (book_id) REFERENCES Books(book_id),
+        quantity DOUBLE
+    )
+    
+""")  
+
+print("DATABASE AND ALL TABLES CREATED SUCCESSFULLY")
